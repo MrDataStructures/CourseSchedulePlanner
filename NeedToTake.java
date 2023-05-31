2e158dad2844ed7ad697b0d8a82ee04b81513a46
MrDataStructures <108447408+MrDataStructures@users.noreply.github.com>
2023-05-31 19:56:38 -0400

    Update NeedToTake.java

--- NeedToTake.java
+++ NeedToTake.java
@@ -2,29 +2,6 @@ package prereqchecker;
 
 import java.util.*;
 
-/**
- * Steps to implement this class main method:
- * 
- * Step 1:
- * AdjListInputFile name is passed through the command line as args[0]
- * Read from AdjListInputFile with the format:
- * 1. a (int): number of courses in the graph
- * 2. a lines, each with 1 course ID
- * 3. b (int): number of edges in the graph
- * 4. b lines, each with a source ID
- * 
- * Step 2:
- * NeedToTakeInputFile name is passed through the command line as args[1]
- * Read from NeedToTakeInputFile with the format:
- * 1. One line, containing a course ID
- * 2. c (int): Number of courses
- * 3. c lines, each with one course ID
- * 
- * Step 3:
- * NeedToTakeOutputFile name is passed through the command line as args[2]
- * Output to NeedToTakeOutputFile with the format:
- * 1. Some number of lines, each with one course ID
- */
 public class NeedToTake {
 
     public static void DFS(String prereq, ArrayList<String> alreadyTaken, HashMap<String, ArrayList<String>> adjList){

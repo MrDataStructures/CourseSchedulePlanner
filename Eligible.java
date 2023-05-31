c190df0bb45f487d3667eb71e85e2a7a1038e395
MrDataStructures <108447408+MrDataStructures@users.noreply.github.com>
2023-05-31 19:56:21 -0400

    Update Eligible.java

--- Eligible.java
+++ Eligible.java
@@ -2,29 +2,6 @@ package prereqchecker;
 
 import java.util.*;
 
-/**
- * 
- * Steps to implement this class main method:
- * 
- * Step 1:
- * AdjListInputFile name is passed through the command line as args[0]
- * Read from AdjListInputFile with the format:
- * 1. a (int): number of courses in the graph
- * 2. a lines, each with 1 course ID
- * 3. b (int): number of edges in the graph
- * 4. b lines, each with a source ID
- * 
- * Step 2:
- * EligibleInputFile name is passed through the command line as args[1]
- * Read from EligibleInputFile with the format:
- * 1. c (int): Number of courses
- * 2. c lines, each with 1 course ID
- * 
- * Step 3:
- * EligibleOutputFile name is passed through the command line as args[2]
- * Output to EligibleOutputFile with the format:
- * 1. Some number of lines, each with one course ID
- */
 public class Eligible {
 
     public static void DFS(String prereq, ArrayList<String> alreadyTaken, HashMap<String, ArrayList<String>> adjList){
